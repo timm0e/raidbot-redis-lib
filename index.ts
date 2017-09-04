6693ed9ce6e2ad3f3305894c4c24902121dc1d9e
Tim <timm0e@bitbucket>
2017-09-04 02:28:45 +0200

    Reformat and Ticket #10

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import ioredis = require("ioredis");
 
 export class Sound {
-
   public id: number;
   public name: string;
   public length: number;
@@ -16,7 +15,6 @@ export class Sound {
 }
 
 export class Category {
-
   public id: number;
   public name: string;
   public membercount: number;
@@ -32,12 +30,10 @@ export class RaidBotDB {
   public readonly RedisClient: ioredis.Redis;
 
   constructor(connname: string, database?: number) {
-    this.RedisClient = new ioredis(
-      {
-        connectionName: connname,
-        db: (database) ? database : 0,
-      },
-    );
+    this.RedisClient = new ioredis({
+      connectionName: connname,
+      db: database ? database : 0,
+    });
 
     this.RedisClient.defineCommand("getSounds", {
       lua: `local members = redis.call("SORT", "sounds", "BY", "sounds:*->name", "ALPHA")
@@ -54,7 +50,7 @@ export class RaidBotDB {
     });
 
     this.RedisClient.defineCommand("getCategories", {
-        lua: `local sort = redis.call('SORT', 'categories', 'BY', 'categories:*:name', 'ALPHA', 'GET', '#')
+      lua: `local sort = redis.call('SORT', 'categories', 'BY', 'categories:*:name', 'ALPHA', 'GET', '#')
       local categorylist = {}
 
       for _, key in ipairs(sort) do
@@ -65,11 +61,11 @@ export class RaidBotDB {
           table.insert(categorylist, cjson.encode(category))
       end
       return categorylist`,
-        numberOfKeys: 0,
-      });
+      numberOfKeys: 0,
+    });
 
     this.RedisClient.defineCommand("getSoundsInCategory", {
-    lua: `local sort = redis.call('SORT', 'categories:' .. ARGV[1] .. ':members', 'BY', 'sounds:*->name', 'ALPHA', 'GET', '#')
+      lua: `local sort = redis.call('SORT', 'categories:' .. ARGV[1] .. ':members', 'BY', 'sounds:*->name', 'ALPHA', 'GET', '#')
     local soundlist = {}
 
     for _, key in ipairs(sort) do
@@ -82,12 +78,11 @@ export class RaidBotDB {
 
     return soundlist
     `,
-    numberOfKeys: 0,
-
-  });
+      numberOfKeys: 0,
+    });
 
     this.RedisClient.defineCommand("getCategoriesForSound", {
-   lua: `local soundcategories = redis.call('SMEMBERS', 'sounds:'..ARGV[1]..':categories')
+      lua: `local soundcategories = redis.call('SMEMBERS', 'sounds:'..ARGV[1]..':categories')
    local categorylist = {}
 
    for _, key in ipairs(soundcategories) do
@@ -100,48 +95,48 @@ export class RaidBotDB {
 
    return categorylist
    `,
-   numberOfKeys: 0,
- });
+      numberOfKeys: 0,
+    });
 
     this.RedisClient.defineCommand("hashToJson", {
-   lua: `local objquery = redis.call('HGETALL', KEYS[1])
+      lua: `local objquery = redis.call('HGETALL', KEYS[1])
    local element = {}
    for i=1,#objquery,2 do element[objquery[i]] = objquery[i+1] end
    return cjson.encode(element)`,
-   numberOfKeys: 1,
- });
+      numberOfKeys: 1,
+    });
 
     this.RedisClient.defineCommand("deleteSound", {
-   lua: `local id = ARGV[1]
+      lua: `local id = ARGV[1]
    redis.call('SREM', 'sounds', id)
    redis.call('ZREMRANGEBYSCORE', 'sounds:nameindex', id, id)
    for _, category in ipairs(redis.call('SMEMBERS', 'categories')) do
        redis.call('SREM', 'categories:' .. category .. ':members', id)
    end
    redis.call('DEL', 'sounds:' .. id, 'sounds:' .. id .. ':categories')`,
-   numberOfKeys: 0,
- });
+      numberOfKeys: 0,
+    });
 
     this.RedisClient.defineCommand("deleteCategory", {
-   lua: `local id = ARGV[1]
+      lua: `local id = ARGV[1]
    redis.call('SREM', 'categories', id)
    for _, sound in ipairs(redis.call('SMEMBERS', 'sounds')) do
        redis.call('SREM', 'sounds:' .. sound .. ':categories', id)
    end
    redis.call('DEL', 'categories:' .. id .. ':members', 'categories:' .. id .. ':name')`,
-   numberOfKeys: 0,
- });
+      numberOfKeys: 0,
+    });
 
     this.RedisClient.defineCommand("deleteJoinsound", {
-  lua: `local id = ARGV[1]
+      lua: `local id = ARGV[1]
   local set = redis.call('HGETALL', 'joinsounds')
   for i=2,#set,2 do
       if set[i] == id then
           redis.call("HDEL", "joinsounds", set[i-1])
       end
   end`,
-  numberOfKeys: 0,
-});
+      numberOfKeys: 0,
+    });
   }
 
   public getSounds(): Promise<Sound[]> {
@@ -162,11 +157,11 @@ export class RaidBotDB {
         }
 
         resolve(sounds);
-    });
+      });
     });
   }
 
-public getCategories(): Promise<Category[]> {
+  public getCategories(): Promise<Category[]> {
     return new Promise((resolve, reject) => {
       (this.RedisClient as any).getCategories((err: any, result: string[]) => {
         if (err) {
@@ -184,34 +179,41 @@ public getCategories(): Promise<Category[]> {
         }
 
         resolve(categories);
-    });
+      });
     });
   }
 
   public getSoundsInCategory(categoryid: number): Promise<Sound[]> {
     return new Promise((resolve, reject) => {
-      (this.RedisClient as any).getSoundsInCategory(categoryid, (err: any, result: string[]) => {
-        if (err) {
-          reject(err);
-        }
-        const sounds = new Array<Sound>();
+      (this
+        .RedisClient as any).getSoundsInCategory(
+        categoryid,
+        (err: any, result: string[]) => {
+          if (err) {
+            reject(err);
+          }
+          const sounds = new Array<Sound>();
 
-        try {
-          result.forEach((soundString) => {
-            sounds.push(JSON.parse(soundString));
-          });
-        } catch (error) {
-          reject(error);
-        }
+          try {
+            result.forEach((soundString) => {
+              sounds.push(JSON.parse(soundString));
+            });
+          } catch (error) {
+            reject(error);
+          }
 
-        resolve(sounds);
-    });
+          resolve(sounds);
+        }
+      );
     });
   }
 
-public getCategoriesForSound(soundid: number): Promise<Category[]> {
+  public getCategoriesForSound(soundid: number): Promise<Category[]> {
     return new Promise((resolve, reject) => {
-        (this.RedisClient as any).getCategoriesForSound(soundid, (err: any, result: string[]) => {
+      (this
+        .RedisClient as any).getCategoriesForSound(
+        soundid,
+        (err: any, result: string[]) => {
           if (err) {
             reject(err);
             return;
@@ -227,117 +229,183 @@ public getCategoriesForSound(soundid: number): Promise<Category[]> {
           }
 
           resolve(categories);
+        }
+      );
+    });
+  }
+
+  public createSound(
+    name: string,
+    length: number,
+    file: string
+  ): Promise<Sound> {
+    return new Promise((resolve, reject) => {
+      this.RedisClient.incr("sounds:id").then((id: number) => {
+        this.RedisClient
+          .multi()
+          .sadd("sounds", id)
+          .hmset(`sounds:${id}`, "name", name, "length", length, "file", file)
+          .zadd("sounds:nameindex", id, name)
+          .exec()
+          .then(() => {
+            resolve(new Sound(id, name, length, file));
+          });
+      });
+    });
+  }
+
+  public addSoundToCategory(
+    soundid: number,
+    categoryid: number
+  ): Promise<void> {
+    return new Promise((resolve, reject) => {
+      Promise.all([
+        this.RedisClient.sismember("categories", categoryid),
+        this.RedisClient.sismember("sounds", soundid),
+      ])
+        .then(
+          this.RedisClient
+            .multi()
+            .sadd(`sounds:${soundid}:categories`, categoryid)
+            .sadd(`categories:${categoryid}:members`, soundid)
+            .exec()
+        )
+        .then(() => {
+          resolve();
         });
     });
   }
 
-public createSound(name: string, length: number, file: string): Promise<Sound> {
+  public getSoundById(soundid: number): Promise<Sound> {
     return new Promise((resolve, reject) => {
-        this.RedisClient.incr("sounds:id").then((id: number) => {
-          this.RedisClient.multi().sadd("sounds", id).hmset(`sounds:${id}`, "name", name, "length", length, "file", file).zadd("sounds:nameindex", id, name).exec().then(() => {
-            resolve(new Sound(id, name, length, file));
-          });
-          });
-  });
-  }
-
-public addSoundToCategory(soundid: number, categoryid: number): Promise<void> {
-    return new Promise((resolve, reject) => {
-      Promise.all([this.RedisClient.sismember("categories", categoryid), this.RedisClient.sismember("sounds", soundid)]).then(this.RedisClient.multi().sadd(`sounds:${soundid}:categories`, categoryid).sadd(`categories:${categoryid}:members`, soundid).exec()).then(() => {resolve(); });
-    });
-  }
-
-public getSoundById(soundid: number): Promise<Sound> {
-    return new Promise((resolve, reject) => {
-        (this.RedisClient as any).hashToJson(`sounds:${soundid}`, (err: any, result: string) => {
-          if (err) {reject(err); return; }
+      (this
+        .RedisClient as any).hashToJson(
+        `sounds:${soundid}`,
+        (err: any, result: string) => {
+          if (err) {
+            reject(err);
+            return;
+          }
 
           try {
             resolve(JSON.parse(result));
           } catch (error) {
-           reject(error);
+            reject(error);
           }
+        }
+      );
+    });
+  }
+
+  public createCategory(name: string): Promise<Category> {
+    return new Promise((resolve, reject) => {
+      this.RedisClient.incr("categories:id").then((id: number) => {
+        return this.RedisClient
+          .multi()
+          .sadd("categories", id)
+          .set(`categories:${id}:name`, name)
+          .exec()
+          .then(resolve(new Category(id, name, 0)));
+      });
+    });
+  }
+
+  public deleteSound(id: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      (this.RedisClient as any).deleteSound(id, () => {
+        (this.RedisClient as any).deleteJoinsound(id, () => {
+          resolve();
         });
+      });
     });
   }
 
-public createCategory(name: string): Promise<Category> {
+  public deleteCategory(id: number): Promise<void> {
     return new Promise((resolve, reject) => {
-        this.RedisClient.incr("categories:id").then((id: number) => {
-          return this.RedisClient.multi().sadd("categories", id).set(`categories:${id}:name`, name).exec().then(resolve(new Category(id, name, 0)));
-        });
+      (this.RedisClient as any).deleteCategory(id, () => {
+        resolve();
+      });
     });
   }
 
-public deleteSound(id: number): Promise<void> {
+  public initializeDB(): Promise<void> {
     return new Promise((resolve, reject) => {
-        (this.RedisClient as any).deleteSound(id, () => {
-          (this.RedisClient as any).deleteJoinsound(id, () => {
-            resolve();
-          });
-        });
+      this.RedisClient.mset("sounds:id", -1, "categories:id", -1).then(() => {
+        resolve();
+      });
     });
   }
 
-public deleteCategory(id: number): Promise<void> {
+  public searchSounds(search: string): Promise<Sound[]> {
     return new Promise((resolve, reject) => {
-      (this.RedisClient as any).deleteCategory(id, () => {resolve(); });
-  });
-  }
-
-public initializeDB(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.RedisClient.mset("sounds:id", -1, "categories:id", -1).then(() => {resolve(); });
-    });
-  }
-
-public searchSounds(search: string): Promise<Sound[]> {
-  return new Promise((resolve, reject) => {
       const searchstring: string = "*" + search.replace(" ", "*") + "*";
-      const stream = this.RedisClient.zscanStream("sounds:nameindex", {match: searchstring});
+      const stream = this.RedisClient.zscanStream("sounds:nameindex", {
+        match: searchstring,
+      });
       const sounds: Sound[] = [];
       const promises: Array<Promise<any>> = [];
 
       stream.on("data", (result: any[]) => {
         result.forEach((element) => {
           if (!isNaN(element)) {
-            promises.push((this.RedisClient as any)
+            promises.push(
+              (this.RedisClient as any)
                 .hashToJson(`sounds:${element}`)
                 .then((json: string) => {
                   const sound: Sound = JSON.parse(json);
                   sound.id = element;
                   sounds.push(sound);
-                }));
+                })
+            );
           }
         });
       });
 
       stream.on("end", () => {
-        Promise.all(promises).then(() => {resolve(sounds); });
+        Promise.all(promises).then(() => {
+          resolve(sounds);
+        });
       });
-  });
-}
+    });
+  }
 
-public setJoinsound(uid: string, sid: number): Promise<void> {
-  return new Promise((resolve, reject) => {
-      this.RedisClient.sismember(sid).then(() => this.RedisClient.hset("joinsounds", uid, sid)).then(resolve).catch(resolve);
-  });
-}
+  public setJoinsound(uid: string, sid: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.RedisClient
+        .sismember("sounds", sid)
+        .then(() => this.RedisClient.hset("joinsounds", uid, sid))
+        .then(resolve)
+        .catch(resolve);
+    });
+  }
 
-public removeJoinsound(uid: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    this.RedisClient.hset(uid).then(resolve);
-  });
-}
+  public removeJoinsound(uid: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.RedisClient.hset(uid).then(resolve);
+    });
+  }
 
-public getJoinsound(uid: string): Promise<Sound> {
-  return new Promise((resolve, reject) => {
+  public getJoinsound(uid: string): Promise<Sound> {
+    return new Promise((resolve, reject) => {
       this.RedisClient
         .hexists("joinsounds", uid)
         .then(() => {
-          this.RedisClient.hget("joinsounds", uid).then((soundid: number) => {this.getSoundById(soundid).then(resolve); });
+          this.RedisClient.hget("joinsounds", uid).then((soundid: number) => {
+            this.getSoundById(soundid).then(resolve);
+          });
         })
         .catch(reject);
-  });
-}
+    });
+  }
+
+  public renameSound(sid: number, name: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.RedisClient.sismember("sounds", sid).then(() =>
+        this.RedisClient
+          .hset("sounds:" + sid, "name", name)
+          .then(resolve)
+          .catch(reject)
+      );
+    });
+  }
 }
